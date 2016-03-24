1358b9655aac26a51987856a9d9d8003e1186c4e
Max Kaye <maxkaye@Maxs-MacBook-Pro-2.local>
2016-03-24 11:09:14 -0400

    project commit with gemfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,28 @@
 require 'csv'
 
-Person.delete_all
+Person.destroy_all
+Contact.destroy_all
 
-CSV.foreach('db/Contacts.csv', headers: true) do |row|
-  Person.create(
+marco = Person.create!(first_name: "Marco", last_name: "Polo", age: "746", ethnicity: "Venetian", origin: "Italy", gender: "male", occupation: "Merchant traveler", company: "self employed", email: "m.polo.church.com")
+max = Person.create!(first_name: "Max", last_name: "Kaye", age: "23", ethnicity: "Russian", origin: "South Florida", gender: "male", occupation: "Student", company: "GA", email: "maximillianchina@gmail.com")
+marco.contacts.create!(last_name: "Columbus", first_name: "Christopher", age: "567", ethnicity: "Portuguese", origin: "Genoa", gender: "male", occupation: "Ship captain / Land Acquirer", company: "Catholic Monarch of Spain", year_met: "1543", place_met: "Fountain of Youth", last_in_contact: "2 years", network_size: "Large")
+
+CSV.foreach('db/contact_data.csv', headers: true) do |row|
+  max.contacts.create!(
     {
-      first_name: row['first_name'],
-      last_name: row['last_name'],
-      age: row['age'],
-      ethnicity: row['ethnicity'],
-      origin: row['place_of_birth'],
-      gender: row['gender'],
-      occupation: row['occupation'],
-      company: row['company'],
-      email: row['email']
+      first_name: row.fetch('First Name'),
+      last_name: row.fetch('Last Name'),
+      age: row.fetch('Age'),
+      ethnicity: row.fetch('Ethnicity'),
+      origin: row.fetch('Origin'),
+      gender: row.fetch('Gender'),
+      occupation: row.fetch('Occupation'),
+      company: row.fetch('Company'),
+      year_met: row.fetch('Year Met'),
+      place_met: row.fetch('Place Met'),
+      last_in_contact: row.fetch('Last in Contact'),
+      network_size: row.fetch('Network Size (L/S)'),
+      email: row.fetch('Email')
     }
   )
 end
